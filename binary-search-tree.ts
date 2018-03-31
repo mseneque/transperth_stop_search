02aa2ed1870ab5d1ed46bb1e0cecfa3d47e07db7
mseneque <mattseneque@gmail.com>
2018-03-31 18:02:14 +0800

    Initial commit

---
+++ binary-search-tree.ts
@@ -0,0 +1,60 @@
+export class BinarySearchTree {
+
+    constructor(public value: any){};
+
+    public left: BinarySearchTree = null;
+    public right: BinarySearchTree = null;
+
+    insert(value) {
+        if (value <= this.value) {
+            if (!this.left) this.left = new BinarySearchTree(value);
+            else this.left.insert(value);
+        }
+        else if (value > this.value) {
+            if (!this.right) this.right = new BinarySearchTree(value);
+            else this.right.insert(value);
+        }
+    }
+
+    contains(value): Boolean {
+        if (this.value === value) return true;
+        if (value < this.value) {
+            if (!this.left) return false;
+            else return this.left.contains(value);
+        }
+        else if (value > this.value) {
+            if (!this.right) return false;
+            else return this.right.contains(value);
+        }
+    }
+
+    depthFirstTraversal(iteratorFunc, order) {
+        if (order === 'pre-order') iteratorFunc(this.value);
+        if (this.left) this.left.depthFirstTraversal(iteratorFunc, order);
+        if (order === 'in-order') iteratorFunc(this.value);
+        if (this.right) this.right.depthFirstTraversal(iteratorFunc, order);
+        if (order === 'post-order') iteratorFunc(this.value);
+    };
+       
+    breadthFirstTraversal(iteratorFunc) {
+        let queue: BinarySearchTree[] = [this];
+        while (queue.length) {
+            let treeNode = queue.shift();
+            iteratorFunc(treeNode);
+            if (treeNode.left) queue.push(treeNode.left);
+            if (treeNode.right) queue.push(treeNode.right);
+        }
+    };
+       
+    getMinVal() {
+        if (this.left) return this.left.getMinVal();
+        else return this.value;
+    };
+       
+    getMaxVal() {
+        if (this.right) return this.right.getMaxVal();
+        else return this.value;
+    };
+
+};
+
